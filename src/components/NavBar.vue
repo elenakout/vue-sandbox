2c60f8311304e3555d4f4f6c6eb75633d075f828
elenakout <elenakout@gmail.com>
2019-08-11 20:17:59 +0300

    navbar layout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,18 @@
 <template>
     <nav>
-        <v-toolbar flat>
-
+        <v-toolbar flat dark color="blue-grey darken-4">
+            <v-toolbar-title>
+                <span class="font-weight-light">GetShit</span>
+                <span>Done</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn text>Dashboard</v-btn>
+                <v-btn text>
+                    <span>Sign Up</span>
+                    <v-icon right>mdi-account-circle-outline</v-icon>
+                </v-btn >
+            </v-toolbar-items>
         </v-toolbar>
     </nav>
 </template>
