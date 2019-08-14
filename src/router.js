0717aaa884935a66f99b5bfe232d5b5a4e639f23
elenakout <elenakout@gmail.com>
2019-08-14 17:55:20 +0300

    signup route

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Dashboard from './views/Dashboard.vue';
 import Projects from './views/Projects.vue';
 import Team from './views/Team.vue';
+import SignUp from './views/SignUp.vue';
 
 Vue.use(Router);
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/team',
       name: 'team',
       component: Team,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp,
     },
   ],
 });
---
+++ src/views/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1 class="display-2">Signup</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
