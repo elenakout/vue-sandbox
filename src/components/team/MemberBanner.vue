94d563eebc83a8dfb9c99cda2dd734d04238d84f
elenakout <elenakout@gmail.com>
2019-09-14 12:32:18 +0300

    hamburger anomation

--- src/components/team/MemberBanner.vue
+++ src/components/team/MemberBanner.vue
@@ -1,60 +1,47 @@
 <template>
-    <div>
-      <h1>banner</h1>
-      <v-img
-      src="https://i.imgur.com/Z2LSTzB.jpg"
-      contain
-      class="grey lighten-2"
-      max-width="100vw"
-      max-height="500"
-      gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
-      >
-        <v-row
-          align="center"
-          justify="space-around"
-          class="lightbox white--text pa-2  fill-height flex">
+  <div>
+    <h1>banner</h1>
+    <v-img src="https://i.imgur.com/Z2LSTzB.jpg" contain class="grey lighten-2" max-width="100vw"
+      max-height="500" gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)">
+      <v-row align="center" justify="space-around"
+        class="lightbox white--text pa-2  fill-height flex">
 
-            <div class="logo">
-              <v-img
-              src="https://i.imgur.com/mDSKItg.png"
-              max-width="80"
-              max-height="80"
-              contain
-              class="logo__img mr-2"
-              ></v-img>
-              <div class="logo__txt">
-                <h3 class="display-1 font-weight-black">INTER</h3>
-                <h4 class="headline font-weight-bold">Club</h4>
-              </div>
+        <div class="logo">
+          <v-img
+            src="https://i.imgur.com/mDSKItg.png"
+            max-width="80"
+            max-height="80"
+            contain
+            class="logo__img mr-2"></v-img>
+          <div class="logo__txt">
+            <h3 class="display-1 font-weight-black">INTER</h3>
+            <h4 class="headline font-weight-bold">Club</h4>
+          </div>
 
-            </div>
-            <div class="display-3 font-weight-black">Προνόμια μελών</div>
-            <div class="action">
-              <v-btn router to="/home" rounded color="primary " large dark>Γινε μελος</v-btn>
-            </div>
-        </v-row>
-      </v-img>
-    </div>
+        </div>
+        <div class="display-3 font-weight-black">Προνόμια μελών</div>
+        <div class="action">
+          <v-btn router to="/home" rounded color="primary " large dark>Γινε μελος</v-btn>
+        </div>
+      </v-row>
+    </v-img>
+  </div>
 </template>
 
-<script>
-export default {
-
-};
-</script>
 
 <style lang="scss" scoped>
-.logo {
-  display: flex;
-
-  &__img {
-    border-radius: 50%;
-  }
-
-  &__txt {
+  .logo {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
+
+    &__img {
+      border-radius: 50%;
+    }
+
+    &__txt {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
   }
-}
+
 </style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,103 +1,18 @@
 <template>
-    <div class="container">
-      <v-stepper v-model="e1">
-        <v-stepper-header>
-          <v-stepper-step :complete="e1 > 1" step="1">Card</v-stepper-step>
-
-          <v-divider></v-divider>
-
-          <v-stepper-step :complete="e1 > 2" step="2">Name of step 2</v-stepper-step>
-
-          <v-divider></v-divider>
-
-          <v-stepper-step step="3">Name of step 3</v-stepper-step>
-        </v-stepper-header>
-
-        <v-stepper-items>
-          <v-stepper-content step="1">
-            <v-card
-              class="mb-12 md5"
-              flat
-            >
-
-
-                        <v-card-text>
-                            <v-form
-                            ref="formin"
-
-                            lazy-validation
-                            >
-                            <v-text-field
-
-                            label="Email"
-                            required
-                            ></v-text-field>
-
-                            <v-text-field
-
-                            type="password"
-                            label="Password"
-                            hint="At least 6 characters"
-                            class="input-group--focused"
-                            :counter="6"
-                            required
-                            ></v-text-field>
-
-                            </v-form>
-                        </v-card-text>
-            </v-card>
-
-            <v-btn
-              color="primary"
-              @click="e1 = 2"
-            >
-              Continue
-            </v-btn>
-
-            <v-btn text @click="e1 = 3">Cancel</v-btn>
-          </v-stepper-content>
-
-          <v-stepper-content step="2">
-            <v-card
-              class="mb-12"
-              color="grey lighten-1"
-              height="200px"
-            ></v-card>
-
-            <v-btn
-              color="primary"
-              @click="e1 = 3"
-            >
-              Continue
-            </v-btn>
-
-            <v-btn text>Cancel</v-btn>
-          </v-stepper-content>
-
-          <v-stepper-content step="3">
-            <v-card
-              class="mb-12"
-              color="grey lighten-1"
-              height="200px"
-            ></v-card>
-
-            <v-btn
-              color="primary"
-              @click="e1 = 1"
-            >
-              Continue
-            </v-btn>
-
-            <v-btn text>Cancel</v-btn>
-          </v-stepper-content>
-        </v-stepper-items>
-      </v-stepper>
+    <div class="container2">
+      <h1 class="center-content">Little Components</h1>
+      <hambuger></hambuger>
     </div>
 </template>
 
 <script>
+import Hambuger from '@/components/projects/hamburger-animation.vue';
+
 export default {
   name: 'projects',
+  components: {
+    Hambuger,
+  },
   data: () => ({
     e1: 0,
   }),
@@ -105,5 +20,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 
 </style>
