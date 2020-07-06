1a920923ae338cfe8bae49574badfd7ae2e4dc57
elenakout <elenakout@gmail.com>
2020-07-06 11:20:19 +0300

    [test] vue2 editor

---
+++ src/components/team/editor.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="editor">
+    <vue-editor v-model="content" />
+    <h1 class="mt-10">Preview</h1>
+    <p>{{ content }}</p>
+    <p v-html="content"></p>
+  </div>
+</template>
+
+<script>
+import { VueEditor } from 'vue2-editor';
+
+export default {
+  components: { VueEditor },
+  data: () => ({
+    content: '<p>Some initial content</p>',
+  }),
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style scoped lang="scss">
+.editor {
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,14 +1,26 @@
 <template>
-  <div>
-    <h1 class="display-2">team</h1>
+  <div class="team">
+    <h1 class="display-2 mb-6">team</h1>
+    <editor />
   </div>
 </template>
 
 <script>
+import Editor from '@/components/team/editor.vue';
+
 export default {
   name: 'team',
-  components: {},
+  components: {
+    Editor,
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.team {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
