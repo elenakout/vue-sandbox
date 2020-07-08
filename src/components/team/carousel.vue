5247a0073af86f23ea6b75e273cb081fb113d18f
elenakout <elenakout@gmail.com>
2020-07-08 10:02:33 +0300

    [test] team - carousel

---
+++ src/components/team/carousel.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="carousel">
+    <v-carousel cycle="true" hide-delimiter-background="true" height="85vh">
+      <v-carousel-item
+        v-for="item in slides"
+        :key="item.id"
+        :src="item.img"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+        class="carItem"
+      ></v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    slides: [
+      {
+        id: 1,
+        img:
+          'https://q-cf.bstatic.com/images/hotel/max1280x900/199/199700894.jpg',
+      },
+      {
+        id: 4,
+        img:
+          'https://q-cf.bstatic.com/images/hotel/max1280x900/199/199719932.jpg',
+      },
+      {
+        id: 2,
+        img:
+          'https://r-cf.bstatic.com/images/hotel/max1280x900/199/199688544.jpg',
+      },
+      {
+        id: 3,
+        img:
+          'https://q-cf.bstatic.com/images/hotel/max1280x900/199/199689840.jpg',
+      },
+    ],
+  }),
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style scoped lang="scss">
+.carItem {
+  img {
+    object-fit: contain;
+  }
+}
+</style>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,16 +2,19 @@
   <div class="team">
     <h1 class="display-2 mb-6">team</h1>
     <editor />
+    <carousel />
   </div>
 </template>
 
 <script>
 import Editor from '@/components/team/editor.vue';
+import Carousel from '@/components/team/carousel.vue';
 
 export default {
   name: 'team',
   components: {
     Editor,
+    Carousel,
   },
 };
 </script>
