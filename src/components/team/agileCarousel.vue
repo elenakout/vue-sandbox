fe61cc2283ccd959ae5ff35f78bc9f36d68f0243
elenakout <elenakout@gmail.com>
2020-07-08 12:57:07 +0300

    [test] vue agile options

--- src/components/team/agileCarousel.vue
+++ src/components/team/agileCarousel.vue
@@ -1,22 +1,9 @@
 <template>
   <div class="agileCarousel">
-    <agile
-      :nav-buttons="false"
-      :autoplay-speed="5000"
-      :speed="2500"
-      fade
-      pause-on-hover
-      pause-on-dots-hover
-      autoplay
-      centerMode
-    >
-      <img
-        class="slide"
-        src="https://q-cf.bstatic.com/images/hotel/max1280x900/199/199700894.jpg"
-        alt
-      />
+    <agile :nav-buttons="false" :autoplay-speed="5000" :speed="2500" fade autoplay centerMode>
+      <img v-for="item in slides" :key="item.id" class="slide" :src="item.img" alt />
 
-      <img
+      <!-- <img
         class="slide"
         src="https://q-cf.bstatic.com/images/hotel/max1280x900/199/199719932.jpg"
         alt
@@ -32,21 +19,8 @@
         src="https://q-cf.bstatic.com/images/hotel/max1280x900/199/199689840.jpg"
         alt="img"
         class="slide"
-      />
+      />-->
     </agile>
-    <div>
-      <ul class="agile__dots">
-        <li class="agile__dot">
-          <button type="button">1</button>
-        </li>
-        <li class="agile__dot">
-          <button type="button">1</button>
-        </li>
-        <li class="agile__dot">
-          <button type="button">1</button>
-        </li>
-      </ul>
-    </div>
   </div>
 </template>
 
@@ -57,45 +31,36 @@ export default {
   components: {
     agile: VueAgile,
   },
-  data: () => ({}),
+  data: () => ({
+    slides: [
+      {
+        id: 1,
+        img:
+          'https://q-cf.bstatic.com/images/hotel/max1280x900/199/199700894.jpg',
+      },
+      {
+        id: 4,
+        img:
+          'https://q-cf.bstatic.com/images/hotel/max1280x900/199/199719932.jpg',
+      },
+      {
+        id: 2,
+        img:
+          'https://r-cf.bstatic.com/images/hotel/max1280x900/199/199688544.jpg',
+      },
+      {
+        id: 3,
+        img:
+          'https://q-cf.bstatic.com/images/hotel/max1280x900/199/199689840.jpg',
+      },
+    ],
+  }),
   methods: {},
   computed: {},
 };
 </script>
 
 <style scoped lang="scss">
-.agile {
-  &__dots {
-    bottom: 10px;
-    flex-direction: column;
-    right: 30px;
-    position: absolute;
-  }
-
-  &__dot {
-    margin: 5px 0;
-  }
-
-  button {
-    background-color: transparent;
-    border: 1px solid #fff;
-    cursor: pointer;
-    display: block;
-    height: 10px;
-    font-size: 0;
-    line-height: 0;
-    margin: 0;
-    padding: 0;
-    transition-duration: 0.3s;
-    width: 10px;
-
-    &--current,
-    &:hover button {
-      background-color: #fff;
-    }
-  }
-}
-
 .slide {
   display: block;
   height: 85vh;
